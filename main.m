ebf7887beb0545d8bc10d4c7a6317e2ef5d6cb75
farhang-arch <farhang.yeganegi@aut.ac.ir>
2020-02-18 20:44:42 +0330

    comment some codes

--- main.m
+++ main.m
@@ -31,28 +31,28 @@ imgout_new=medfilt2(thresh_func(imgout,6,5,c+0.07));
 pad=@(x,c) (3125*(x-c).^6 + 13125*(x-c).^5 + 26250*(x-c).^4 + ...
     31500*(x-c).^3 + 23625*(x-c).^2 + 10395*(x-c) + 2079)./ ...
     (2*(3125*(x-c).^6 + 26250*(x-c).^4 + 23625*(x-c).^2 + 2079));
-% % % % % type=input('insert a type = ');
-% % % % % tic
-% % % % % switch type
-% % % % %     case 'sig_orig'
-% % % % %         imgout_new=sigmoid(imgout,c+0.1,10);
-% % % % %     case 'sig_pade_app'
-% % % % %         imgout_new=pad(imgout,c);
-% % % % % end
-% % % % % toc
-% % % % % % imwrite(ynew,'E:\paper1 code data\bsc_result\15_g.jpg')
-% % % % % originalvessel=impixel(imggreen);
-% % % % % originalbackground=impixel(imggreen);
-% % % % % % grayscale cef
-% % % % % origves=originalvessel(:,1)';
-% % % % % origback=originalbackground(:,1)';
-% % % % % n=length(origves);
-% % % % % cefden=(1/n)*abs(sum(origves)-sum(origback));
-% % % % % % Contrast Streching Based
-% % % % % enhancedvessely=impixel(imgout_new);
-% % % % % enhancedvesy=enhancedvessely(:,1)';
-% % % % % enhancedbackgroundy=impixel(imgout_new);
-% % % % % enhancedbacky=enhancedbackgroundy(:,1)';
-% % % % % cefnumy=(1/n)*abs(sum(enhancedvesy)-sum(enhancedbacky));
-% % % % % cefproposedy=cefnumy/cefden;
-
+% % % % % % type=input('insert a type = ');
+% % % % % % tic
+% % % % % % switch type
+% % % % % %     case 'sig_orig'
+% % % % % %         imgout_new=sigmoid(imgout,c+0.1,10);
+% % % % % %     case 'sig_pade_app'
+% % % % % %         imgout_new=pad(imgout,c);
+% % % % % % end
+% % % % % % toc
+% % % % % % % imwrite(ynew,'E:\paper1 code data\bsc_result\15_g.jpg')
+% % % % % % originalvessel=impixel(imggreen);
+% % % % % % originalbackground=impixel(imggreen);
+% % % % % % % grayscale cef
+% % % % % % origves=originalvessel(:,1)';
+% % % % % % origback=originalbackground(:,1)';
+% % % % % % n=length(origves);
+% % % % % % cefden=(1/n)*abs(sum(origves)-sum(origback));
+% % % % % % % Contrast Streching Based
+% % % % % % enhancedvessely=impixel(imgout_new);
+% % % % % % enhancedvesy=enhancedvessely(:,1)';
+% % % % % % enhancedbackgroundy=impixel(imgout_new);
+% % % % % % enhancedbacky=enhancedbackgroundy(:,1)';
+% % % % % % cefnumy=(1/n)*abs(sum(enhancedvesy)-sum(enhancedbacky));
+% % % % % % cefproposedy=cefnumy/cefden;
+% 
